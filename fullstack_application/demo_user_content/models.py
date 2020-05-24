f9392b56288ef03339ab8e5dfc18872e0b6ca2b1
Diganta Datta <diganta.datta.3@gmail.com>
2020-05-24 19:28:38 +0530

    created migration for demo user content

--- fullstack_application/demo_user_content/models.py
+++ fullstack_application/demo_user_content/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class DemoUserContent(models.Model):
+ name= models.CharField(max_length=100)
+ email=models.EmailField(max_length=100,unique=True)
+ message=models.CharField(max_length=200,blank=True)
+ created_at=models.DateTimeField(auto_now_add=True)
