0ca337f4febf90b73f3cba57c43f9288717f8948
Diganta Datta <diganta.datta.3@gmail.com>
2020-05-24 21:06:47 +0530

    React front end setup with semantic UI linking

---
+++ fullstack_application/frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>React app</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("combinativ_app"));
