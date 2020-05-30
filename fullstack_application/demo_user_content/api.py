e9d698acdaa968dcda2eac3fb2ebc9cf2077e115
Diganta Datta <diganta.datta.3@gmail.com>
2020-05-30 15:19:58 +0530

    Issue resolved

--- fullstack_application/demo_user_content/api.py
+++ fullstack_application/demo_user_content/api.py
@@ -1,15 +1,20 @@
 from demo_user_content.models import DemoUserContent
-from rest_framework import viewsets,permissions
+from rest_framework import viewsets, permissions
 from .serializers import DemoUserContentSerializer
 
-#demo user content viewset
+# demo user content viewset
+
+
 class DemoUserContentViewset(viewsets.ModelViewSet):
- queryset=DemoUserContent.objects.all()
- permission_classes=[
-  permissions.IsAuthenticated
- ]
- serializer_class=DemoUserContentSerializer
- def get_queryset(self):
-  return self.request.user.DemoUserContent.all() #returns all the content owned by user
- def perform_create(self, serializer):
-  serializer.save(owner=self.request.user)
+    queryset = DemoUserContent.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = DemoUserContentSerializer
+
+    def get_queryset(self):
+        # returns all the content owned by user
+        return self.request.user.contents.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
