796c59f0f64409d2e98a89465e39bee04ec90e82
diganto <diganta.datta.3@gmail.com>
2020-10-15 12:02:33 +0530

    updated webpack config for jsx and css style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,68 @@
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+      // {
+      // 	test: /\.(png|jpg)$/,
+      // 	exclude: /(node_modules)/,
+      // 	loader: "file-loader",
+      // },
+      // {
+      // 	test: /\.(png|jpg|gif|svg)$/,
+      // 	loader: "file-loader",
+      // 	options: {
+      // 		publicPath: "static/",
+      // 		outputPath: (url, resourcePath, context) => {
+      // 			// `resourcePath` is original absolute path to asset
+      // 			// `context` is directory where stored asset (`rootContext`) or `context` option
+
+      // 			// To get relative path you can use
+      // 			//const relativePath = path.relative(context, resourcePath);
+
+      // 			if (/my-custom-image\.png/.test(resourcePath)) {
+      // 				return `other_output_path/${url}`;
+      // 			}
+
+      // 			if (/images/.test(context)) {
+      // 				return `image_output_path/${url}`;
+      // 			}
+
+      // 			return `${url}`;
+      // 		},
+      // 	},
+      // },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 };
